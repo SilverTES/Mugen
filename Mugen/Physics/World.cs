8ac5bfda6ad838c203cca3dbba9671a2a628acef
silvertes.pro <silvertes.pro@gmail.com>
2024-08-31 12:28:36 +0200

    Add World.Direction Point const.

--- Mugen/Physics/World.cs
+++ Mugen/Physics/World.cs
@@ -544,6 +544,24 @@ namespace Mugen.Physics
         DIAGONAL,
     }
 
+    public static class Direction
+    {
+        public static readonly Point Up = new(0, -1);
+        public static readonly Point Down = new(0, 1);
+        public static readonly Point Left = new(-1, 0);
+        public static readonly Point Right = new(1, 0);
+
+        public static readonly Point UpLeft = new(-1, -1);
+        public static readonly Point DownLeft = new(-1, 1);
+        public static readonly Point UpRight = new(1, -1);
+        public static readonly Point DownRight = new(1, 1);
+
+        public static readonly Point LeftUp = UpLeft;
+        public static readonly Point LeftDown = DownLeft;
+        public static readonly Point RightUp = UpRight;
+        public static readonly Point RightDown = DownRight;
+    }
+
     public static class Geo
     {
 
