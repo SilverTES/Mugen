8e88660b746cecffbc84a17705c39d967421e8af
silvertes.pro <silvertes.pro@gmail.com>
2025-03-25 10:57:34 +0100

    GFX.Init() is public static now, no need to WindowManager.Init() for use GFX methods.

--- Mugen/GFX/GFX.cs
+++ Mugen/GFX/GFX.cs
@@ -110,7 +110,7 @@ namespace Mugen.GFX
         static public Texture2D? _defaultSkinGui = null;
         static public Texture2D? _gamePadSNES = null;
 
-        internal static void Init(GraphicsDevice graphicsDevice) //, SpriteFont defaultFont)
+        public static void Init(GraphicsDevice graphicsDevice) //, SpriteFont defaultFont)
         {
             //_defaultFont = defaultFont;
 
