aad5c48d787ea5e0b7542ecf40b8879d6855fbce
silvertes.pro <silvertes.pro@gmail.com>
2025-04-27 18:45:27 +0200

    Add class Shake

--- Mugen/Core/Misc.cs
+++ Mugen/Core/Misc.cs
@@ -197,6 +197,46 @@ namespace Mugen.Core
         }
     }
 
+    public class Shake
+    {
+        float _intensity;
+        float _step;
+        public bool IsShake { get; private set; }
+        bool _shakeX;
+        bool _shakeY;
+
+        public void SetIntensity(float intensity, float step = 2, bool shakeX = true, bool shakeY = true)
+        {
+            _intensity = intensity;
+            _step = step;
+            IsShake = true;
+            _shakeX = shakeX;
+            _shakeY = shakeY;
+        }
+
+        public Vector2 GetVector2()
+        {
+            Vector2 vec = new Vector2();
+
+            if (_intensity > 0)
+            {
+                IsShake = true;
+
+                vec.X = _shakeX ? Misc.Rng.Next(-(int)_intensity, (int)_intensity) : 0;
+                vec.Y = _shakeY ? Misc.Rng.Next(-(int)_intensity, (int)_intensity) : 0;
+
+                _intensity -= _step;
+
+            }
+            else
+            {
+                IsShake = false;
+            }
+
+            return vec;
+        }
+    }
+
     public class EasingValue
     {
         public float Value { get; set; }
